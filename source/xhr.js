009751dad8741c21f34245a7d179b3773f9aefbb
thefarside <thefarside@users.noreply.github.com>
2018-06-13 23:14:06 -0700

    Update xhr.js

--- source/xhr.js
+++ source/xhr.js
@@ -7,24 +7,29 @@ HTML:
 
 CSS:
 #progress {
-	height: 3px;
-	background: #000;
-	transition: width .4s ease 0s;
-	position: fixed;
-	top: 0;
-	z-index: 9999;
+height: 3px;
+background: #000;
+transition: width .4s ease 0s;
+position: fixed;
+top: 0;
+z-index: 9999;
 }
 
 */
 
-xhr.onprogress = function(e) {
-	if (e.lengthComputable) {
-		jQuery( '#progress' ).css({ width: e.loaded / e.total * 100 + '%' });
+var progressBar = document.getElementById( 'progress' );
+xhr.onloadstart = function() {
+	if ( progressBar) {
+		progressBar.style.cssText = 'opacity: 1; transition: opacity 1.3s; width: 0%;';
 	}
 };
-xhr.onloadstart = function() {
-	jQuery( '#progress' ).css({ 'opacity': '1' });
+xhr.onprogress = function( e ) {
+	if ( e.lengthComputable && progressBar ) {
+		progressBar.style.cssText = 'opacity: 1; transition: opacity 1.3s; width: ' + e.loaded / e.total * 100 + '%;';
+	}
 };
 xhr.onloadend = function() {
-	jQuery( '#progress' ).css({ 'opacity': '0', 'transition': 'opacity 1.3s' });
+	if ( progressBar) {
+		progressBar.style.cssText = 'opacity: 0; transition: opacity 1.3s; width: 100%;';
+	}
 };
